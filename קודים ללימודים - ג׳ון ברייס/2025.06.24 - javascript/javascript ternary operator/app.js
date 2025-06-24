eafd2b24366f3204c8a14aef9c5f96d222ba37cc
Dean Stark <deanstark04@gmail.com>
2025-06-24 13:56:54 +0300

    ternary

--- קודים ללימודים - ג׳ון ברייס/2025.06.24 - javascript/javascript ternary operator/app.js
+++ קודים ללימודים - ג׳ון ברייס/2025.06.24 - javascript/javascript ternary operator/app.js
@@ -11,9 +11,9 @@
 // it's another syntax for if...
 // boolean expression ? value for true : value for false
 
-// price = age <= 18 ? 20 : 30;
+price = age <= 18 ? 20 : 30;
 
-// alert(`your ticket price is ${price}`)
+alert(`your ticket price is ${price}`)
 
 // const num1 = +prompt("please enter a number")
 // const num2 = +prompt("please enter another number")
@@ -22,22 +22,3 @@
 // max = num1 >= num2 ? num1 : num2;
 
 // alert(`the largest number is ${max}`);
-
-// question 1 > let num = 0
-// while(num <= 100) {
-//     document.write(`${num} \n`);
-//     num++
-// }
-
-// question 2 > let num = 2;
-// while(num <= 100) {
-//     document.write(`${num} \n`)
-//     num = num + 2;
-// }
-
-let num = 100;
-while(num >= 2) {
-    document.write(`${num} \n`);
-    num -= 2;
-}
-
