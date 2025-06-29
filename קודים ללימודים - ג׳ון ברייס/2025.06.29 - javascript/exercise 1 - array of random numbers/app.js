a59291fa2e36304a91bb9ac3ba22e1c5aa5088d9
Dean Stark <deanstark04@gmail.com>
2025-06-29 18:32:43 +0300

    array of random numbers fix

--- קודים ללימודים - ג׳ון ברייס/2025.06.29 - javascript/exercise 1 - array of random numbers/app.js
+++ קודים ללימודים - ג׳ון ברייס/2025.06.29 - javascript/exercise 1 - array of random numbers/app.js
@@ -12,9 +12,10 @@
 
 const randomNumbers = [];
 
-for (let i = 1; i <= 20; i++)
-  randomNumbers.push((parseInt(Math.random * (80 - 20 + 1)) + 20));
-
-for (const randomNum of randomNumbers) console.log(randomNumbers);
+for (let i = 1; i <= 20; i++) {
+  randomNumbers.push(parseInt(Math.random() * (80 - 20 + 1) + 20));
+  console.log(randomNumbers[i])
+}
+// for (const randomNum of randomNumbers) console.log(randomNumbers);
   
 
