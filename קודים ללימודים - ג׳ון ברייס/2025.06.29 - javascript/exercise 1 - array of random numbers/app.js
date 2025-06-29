22078e8f4c288f87c213a070fdde1f0b0175765a
Dean Stark <deanstark04@gmail.com>
2025-06-29 15:54:32 +0300

    random numbers array

---
+++ קודים ללימודים - ג׳ון ברייס/2025.06.29 - javascript/exercise 1 - array of random numbers/app.js
@@ -0,0 +1,20 @@
+// const numbers = [];
+// const max = 80;
+// const min = 20;
+// let random = Math.random(parseInt(random * (max - min + 1)) + min)
+
+// for (let i = 1; i <= 20; i++) {
+//   random = Math.random(parseInt(random * (max - min + 1)) + min)
+//   numbers.push(random);
+// }
+
+// console.log(numbers);
+
+const randomNumbers = [];
+
+for (let i = 1; i <= 20; i++)
+  randomNumbers.push((parseInt(Math.random * (80 - 20 + 1)) + 20));
+
+for (const randomNum of randomNumbers) console.log(randomNumbers);
+  
+
---
+++ קודים ללימודים - ג׳ון ברייס/2025.06.29 - javascript/pushing values to arrays/app.js
@@ -0,0 +1,8 @@
+const grades = [];
+
+for (let i = 1; i <= 5; i++) {
+  // grades[i] = +prompt(`please enter grade`);
+  grades.push(+prompt(`please enter grade`));
+}
+
+console.log(grades);
