3cd922300fa90a6bc82f133bca55faee9e5e8450
Dean Stark <deanstark04@gmail.com>
2025-07-05 12:23:05 +0300

    matrix hw 3 completed

--- שיעורי בית - ג׳ון ברייס/2025.07.01/matrix/hw 3/app.js
+++ שיעורי בית - ג׳ון ברייס/2025.07.01/matrix/hw 3/app.js
@@ -14,31 +14,32 @@ for (let row = 0; row < products.length; row++) {
 
 let maxLength = products[0][0].length;
 let minLength = products[0][0].length;
-const maxLengthArray = [];
-const minLengthArray = [];
-let longItem;
-let shortItem;
+let maxProducts = [];
+let minProducts = [];
 
 for (let row = 0; row < products.length; row++) {
   for (let col = 0; col < products[row].length; col++) {
     let currentItem = products[row][col];
-    if (currentItem.length > maxLength) {
-      maxLengthArray.push(currentItem);
-      longItem = currentItem;
-    } else if (currentItem.length === maxLength && !currentItem.includes(products[row][col])) {
-      maxLengthArray.push(currentItem);
-    } else if (currentItem.length < minLength) {
-      minLengthArray.push(currentItem);
-      shortItem = currentItem;
-    } else if (currentItem.length === minLength && !currentItem.includes(products[row][col])) {
-      minLengthArray.push(currentItem);
+    let currentLength = currentItem.length;
+    if (currentLength < minLength) {
+      minLength = currentLength;
+      minProducts.length = 0;
+      minProducts.push(currentItem);
+    } else if (currentLength === minLength) {
+      minProducts.push(currentItem);
+    };
+    if (currentLength > maxLength) {
+      maxLength = currentLength;
+      maxProducts.length = 0;
+      maxProducts.push(currentItem);
+    } else if (currentLength === maxLength) {
+      maxProducts.push(currentItem);
     }
   }
-  document.write(`<br>`);
 };
 
-document.write(`<br>`);
-document.write(`the products with the longest names are: ${maxLengthArray}`);
-document.write(`<br>`);
-document.write(`the products with the shortest names are: ${minLengthArray}`);
+
+document.write(`<br>the longest items are: ${maxProducts}, `);
+document.write(`<br>the shortest items are: ${minProducts}, `);
+
 
