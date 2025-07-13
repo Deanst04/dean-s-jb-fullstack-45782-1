5ad916086758e85b94eed0561116deec20b93dca
Dean Stark <deanstark04@gmail.com>
2025-07-13 15:31:09 +0300

    idk

--- קודים ללימודים - ג׳ון ברייס/2025.07.13 - project 1 prep/01 - project 1 prep/app.js
+++ קודים ללימודים - ג׳ון ברייס/2025.07.13 - project 1 prep/01 - project 1 prep/app.js
@@ -1,21 +1,22 @@
-const CARS_KEY_NAME = `cars`;
+const CARS_KEY_NAME = "cars";
 
 function addCar(event) {
-
   event.preventDefault(); // prevent form submission
   const data = collectDataFromForm();
-  const newTR = generateTR(data);
+  const carsJSON = localStorage.getItem(CARS_KEY_NAME);
+  const carId = carsJSON ? JSON.parse(carsJSON).length : 0;
+  const newTR = generateTR(data, carId);
   injectTRToDOM(newTR);
-  saveCarsToLocalStorage(data);
+  saveCarToLocalStorage(data);
   clearForm();
 }
 
 function collectDataFromForm() {
-  const name = document.getElementById(`car-name`).value
-  const engineVolume = document.getElementById(`engine-vol`).value
-  const color = document.getElementById(`car-color`).value
-  const pic = document.getElementById(`car-pic`).value
-  
+  const name = document.getElementById("car-name").value;
+  const engineVolume = document.getElementById("engine-volume").value;
+  const color = document.getElementById("car-color").value;
+  const pic = document.getElementById("car-pic").value;
+
   return {
     name,
     engineVolume,
@@ -24,43 +25,58 @@ function collectDataFromForm() {
   };
 }
 
-function generateTR(data) {
+function generateTR(data, id) {
+  // deconstruction
+  const { color, name, engineVolume, pic } = data;
+console.log(color)
   const newTR = `
-  <tr style = "background-color: ${data.color};">
-      <td>${data.name}</td>
-      <td>${data.engineVolume}</td>
-      <td>
-      <img class="car-pic" src="${data.pic}">
-      </td>
-  </tr>
-  `;
+        <tr >
+            <td style="background-color: ${color};">${name}</td>
+            <td>${engineVolume}</td>
+            <td><img class="car-pic"
+                    src="${pic}">
+            </td>
+            <td><button onclick="deleteCar(${id})">delete</button></td>
+        </tr>
+    `;
   return newTR;
 }
 
 function injectTRToDOM(newTR) {
-  document.getElementById(`car-list`).innerHTML += newTR;
+  document.getElementById("car-list").innerHTML += newTR;
 }
 
 function loadCarsFromStorage() {
+  document.getElementById("car-list").innerHTML = "";
   const carsJSON = localStorage.getItem(CARS_KEY_NAME);
   if (carsJSON) {
-  const cars = JSON.parse(carsJSON);
-  for (const car of cars) {
-      const newTR = generateTR(car);
+    const cars = JSON.parse(carsJSON);
+    let i = 0;
+    for (const car of cars) {
+      const newTR = generateTR(car, i);
+      i++;
       injectTRToDOM(newTR);
     }
   }
 }
 
-function saveCarsToLocalStorage(car) {
-  const carsJSON = localStorage.getItem(CARS_KEY_NAME) || `[]`;
+function saveCarToLocalStorage(car) {
+  const carsJSON = localStorage.getItem(CARS_KEY_NAME) || "[]";
   const cars = JSON.parse(carsJSON);
   cars.push(car);
   localStorage.setItem(CARS_KEY_NAME, JSON.stringify(cars));
 }
 
 function clearForm() {
-  document.getElementById(`car-form`).reset();
+  document.getElementById("car-form").reset();
+}
+
+function deleteCar(id) {
+  const carsJSON = localStorage.getItem(CARS_KEY_NAME);
+  const cars = JSON.parse(carsJSON);
+  cars.splice(id, 1);
+  localStorage.setItem(CARS_KEY_NAME, JSON.stringify(cars));
+  loadCarsFromStorage();
 }
 
 loadCarsFromStorage();
