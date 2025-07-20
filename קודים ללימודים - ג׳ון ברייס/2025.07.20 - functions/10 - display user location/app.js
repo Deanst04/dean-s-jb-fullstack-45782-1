e3724b845b9057694c48fdea7d91de90d9dd204f
Dean Stark <deanstark04@gmail.com>
2025-07-20 13:32:00 +0300

    user location

---
+++ קודים ללימודים - ג׳ון ברייס/2025.07.20 - functions/10 - display user location/app.js
@@ -0,0 +1,13 @@
+const showUserLocation = () => {
+  navigator.geolocation.getCurrentPosition(position => { 
+    console.log('inside success callback')
+    console.log(position)
+  }, error => {
+    console.log('inside error callback')
+    console.log(error);
+  })
+  console.log(`immediately after invoking getCurrentLocation`)
+  
+}
+
+showUserLocation();
