d958e70e51dbe825abed5badfb26b6dd84ad97f3
Dean Stark <deanstark04@gmail.com>
2025-07-20 10:07:29 +0300

    anonymous functions

---
+++ קודים ללימודים - ג׳ון ברייס/2025.07.20 - functions/06 - anonymous functions/app.js
@@ -0,0 +1,10 @@
+function somePower(num, func) {
+    if (typeof func !== `function`) alert(`big error, no function found`);
+    console.log(`result of ${num} and ${func} is ${func(num)}`);
+}
+
+somePower(4, function (num) {return num**2});
+
+somePower(4, function (num) {return num**3});
+
+somePower(4, console.log);
