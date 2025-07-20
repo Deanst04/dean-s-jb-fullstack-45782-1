4a971b919e1eea763ff40048d9ccbd88a9c9342f
Dean Stark <deanstark04@gmail.com>
2025-07-20 09:36:32 +0300

    lesson 3

---
+++ קודים ללימודים - ג׳ון ברייס/2025.07.20 - functions/03 - function is like a let var/app.js
@@ -0,0 +1,13 @@
+function showGrade(grade) {
+    console.log(`your grade is ${grade}`)
+    return `yarden`
+}
+
+showGrade(96);
+console.log(showGrade(96));
+
+showGrade = `gold`;
+
+showGrade(96);
+console.log(showGrade(96));
+
