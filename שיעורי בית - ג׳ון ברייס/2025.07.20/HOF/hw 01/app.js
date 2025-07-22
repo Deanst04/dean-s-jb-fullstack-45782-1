24ddef0f2acf2b756f6445c142d901220877eb96
Dean Stark <deanstark04@gmail.com>
2025-07-22 08:41:38 +0300

    HOF hw 1 completed

--- שיעורי בית - ג׳ון ברייס/2025.07.20/HOF/hw 01/app.js
+++ שיעורי בית - ג׳ון ברייס/2025.07.20/HOF/hw 01/app.js
@@ -9,29 +9,41 @@ const random20NumbersArray = () => {
 const randArr = random20NumbersArray();
 console.log(randArr);
 
+console.log(`question 1`)
 randArr.forEach((num, i) => console.log(num));
 console.log(`<===========================>`);
 
+console.log(`question 2`)
 console.log(randArr.find(num => num % 2 === 0));
 console.log(`<===========================>`);
 
+console.log(`question 3`)
 console.log(randArr.find(num => num > 50));
 console.log(`<===========================>`);
 
+console.log(`question 4`)
+console.log(randArr.filter(num => num % 2 !== 0));
+console.log(`<===========================>`);
+
+console.log(`question 5`)
 console.log(randArr.filter(num => num > 50));
 console.log(`<===========================>`);
 
+console.log(`question 6`)
 console.log(randArr.findIndex(num => num > 50));
 console.log(`<===========================>`);
 
+console.log(`question 7`)
 randArr.forEach(num => {
     if (num % 2 === 0) console.log(`even: ${num}`);
     else console.log(`odd: ${num}`);
 });
 console.log(`<===========================>`);
 
+console.log(`question 8`)
 console.log(`The max number is ${randArr.reduce((acc, curr) => curr > acc ? curr : acc, randArr[0])}`);
 console.log(`<===========================>`);
 
+console.log(`question 9`)
 console.log(`The min number is ${randArr.reduce((acc, curr) => curr < acc ? curr : acc, randArr[0])}`);
 console.log(`<===========================>`);
