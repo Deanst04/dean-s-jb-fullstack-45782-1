3732bd9d2b44398121dbe374c1dfff7db6f91bde
Dean Stark <deanstark04@gmail.com>
2025-07-22 09:05:05 +0300

    callback arrow functions hw 9

--- שיעורי בית - ג׳ון ברייס/2025.07.20/callback functions/arrow functions/hw 09/app.js
+++ שיעורי בית - ג׳ון ברייס/2025.07.20/callback functions/arrow functions/hw 09/app.js
@@ -1,16 +1,21 @@
-const colors = [
-  "red",
-  "blue",
-  "green",
-  "yellow",
-  "purple",
-  "orange",
-  "pink",
-  "teal",
-  "maroon",
-  "navy",
-];
+// const colors = [
+//   "red",
+//   "blue",
+//   "green",
+//   "yellow",
+//   "purple",
+//   "orange",
+//   "pink",
+//   "teal",
+//   "maroon",
+//   "navy",
+// ];
+
+const r = () => Math.floor(Math.random() * (256))
+const g = () => Math.floor(Math.random() * (256))
+const b = () => Math.floor(Math.random() * (256))
+const rgb = () => `rgb(${r()}, ${g()}, ${b()})`
 
 setInterval(() =>
-  document.body.style.backgroundColor = colors[parseInt(Math.random() * colors.length)]
+  document.body.style.backgroundColor = rgb()
 , 1 * 1000);
