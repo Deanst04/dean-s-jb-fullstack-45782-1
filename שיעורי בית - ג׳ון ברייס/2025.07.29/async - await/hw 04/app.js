0e78564d32999e1ccc26ee3605342a9034bff267
Dean Stark <deanstark04@gmail.com>
2025-07-31 14:40:29 +0300

    hw 04

--- שיעורי בית - ג׳ון ברייס/2025.07.29/async - await/hw 04/app.js
+++ שיעורי בית - ג׳ון ברייס/2025.07.29/async - await/hw 04/app.js
@@ -2,12 +2,33 @@
 
 (async () => {
 
-    
     const getData = url => fetch(url).then(result => result.json())
-    
-    const fetchUsers = async () => {
-        const users = await getData(`https://dummyjson.com/recipes`)
-        return users
-    }
 
+
+    document.getElementById("get-user").addEventListener(`click`, async () => {
+        
+        const fetchUsers = async () => {
+            const { recipes } = await getData(`https://dummyjson.com/recipes`)
+            return recipes
+        }
+
+        const generateRecipesData = recipes => {
+            const html = recipes.map(({ name, image }) => `
+            <tr>
+            <td class="text-center align-middle">${name}</td>
+            <td class="text-center align-middle"><img src="${image}"></td>
+            </tr>
+            `).join(``)
+            return html
+        }
+        
+        try {
+            const recipes = await fetchUsers()
+            const html = generateRecipesData(recipes)
+            document.getElementById("recipes-table").innerHTML = html
+            console.log(`success`)
+        } catch (err) {
+            console.log(`error: ${err}`)
+        }
+    })
 })()
