be1831bcc0f8ceb37fc23fe04113aab42538604f
Dean Stark <deanstark04@gmail.com>
2025-08-05 16:02:03 +0300

    types

---
+++ קודים ללימודים - ג׳ון ברייס/2025.08.05/07 - types/js/app.js
@@ -0,0 +1,42 @@
+"use strict";
+// primitives
+Object.defineProperty(exports, "__esModule", { value: true });
+const age = 23;
+const message = `hello world`;
+const isActive = true;
+// arrays
+const ages = [23, 42, 66];
+const messages = [`hello`, `world`];
+const someFlags = [true, false, true];
+const mixedArray = [1, `hello`, true]; // unknown is a little better than any
+// objects
+const myObj = {};
+const aUser = {
+    name: `uval`,
+    age: 22,
+    birthDate: new Date()
+};
+const bUser = {
+    name: `shlomo`,
+    age: 35,
+    birthDate: new Date()
+};
+const users = [];
+users.push(aUser);
+users.push(bUser);
+const someVar = `cxvxcv`;
+const someVar2 = 88;
+const mitzi = {
+    name: `mitzi`,
+    numberOfSoulsLeft: 3,
+};
+const animal = {
+    name: `mitzi`,
+    numberOfSoulsLeft: 5,
+    breed: ``
+};
+const lion = {
+    name: `leo`,
+    eat: () => { }
+};
+//# sourceMappingURL=app.js.map
