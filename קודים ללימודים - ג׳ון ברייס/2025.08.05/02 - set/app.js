14be756ee9abb97ac9ae768ee0f8725650a68e82
Dean Stark <deanstark04@gmail.com>
2025-08-05 10:22:53 +0300

    set

--- קודים ללימודים - ג׳ון ברייס/2025.08.05/02 - set/app.js
+++ קודים ללימודים - ג׳ון ברייס/2025.08.05/02 - set/app.js
@@ -19,7 +19,26 @@
     console.log([90, 96, 72])
     console.log(`the grade 96 is present in the Set? ${myGrades.has(96)}`)
 
-    // 
+    // here is an array:
+    const grades = [78, 82, 88, 88, 90, 92, 92, 96, 96, 96, 96, 99]
 
-    
+    // create an array from grades with only uniques values
+    console.log(
+        grades.reduce((acc, currGrade) => {
+        const current = [...acc]
+        const isInArray = current.includes(currGrade)
+        if (!isInArray) current.push(currGrade)
+
+        return current
+    }, [])
+    )
+
+    // use Set to do same in a single command!
+    // clues:
+    // 1. a set can be initiated with an array e.g. const mySet = new Set(myArray) 
+    // 2. ...
+
+    const uniqueArr = [...new Set(grades)] // the elegant way to unique an array in javascript
+    console.log(uniqueArr)
+
 })()
