87fd8d465a6aafc2eb96aa4014fe87b0d37adfa5
Dean Stark <deanstark04@gmail.com>
2025-08-05 10:08:37 +0300

    set

---
+++ קודים ללימודים - ג׳ון ברייס/2025.08.05/02 - set/app.js
@@ -0,0 +1,25 @@
+"use strict";
+
+(() => {
+
+    const myGrades = new Set()
+
+    // set is a collection
+    // much like an array
+    myGrades.add(90)
+    myGrades.add(96)
+    myGrades.add(72)
+    console.log(myGrades)
+
+    // set can only hold UNIQUE values
+    myGrades.add(72)
+    console.log(myGrades)
+
+    // for performance consideration: set functions work with O(1)
+    console.log([90, 96, 72])
+    console.log(`the grade 96 is present in the Set? ${myGrades.has(96)}`)
+
+    // 
+
+    
+})()
