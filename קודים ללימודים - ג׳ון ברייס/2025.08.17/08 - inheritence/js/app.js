be8f1b71f970130ab3f1f25f5cd3b87a6b661e21
Dean Stark <deanstark04@gmail.com>
2025-08-17 15:17:46 +0300

    inheritance

---
+++ קודים ללימודים - ג׳ון ברייס/2025.08.17/08 - inheritence/js/app.js
@@ -0,0 +1,8 @@
+import Cat from "./cat.js";
+import Dog from "./dog.js";
+const mitzi = new Cat(4, 5);
+mitzi.sayMiau();
+console.log(`mitzi weight ${mitzi.weight}kg`);
+const lucky = new Dog(24, `border collie`);
+lucky.bark();
+console.log(`lucky weight ${lucky.weight}kg`);
