adf1f6283f850791523cd4c0b20735b43b70bf5a
Dean Stark <deanstark04@gmail.com>
2025-08-19 10:44:37 +0300

    abstract example

---
+++ קודים ללימודים - ג׳ון ברייס/2025.08.19/04 - abstarct example/js/app.js
@@ -0,0 +1,16 @@
+import AlertLogger from "./alert.js";
+import ConsoleLogger from "./console.js";
+const logger = new AlertLogger();
+logger.log(`hello world`);
+logger.log(`hello world`);
+logger.log(`hello world`);
+logger.log(`hello world`);
+logger.log(`hello world`);
+logger.log(`hello world`);
+const logger2 = new ConsoleLogger();
+logger2.log(`hello world`);
+logger2.log(`hello world`);
+logger2.log(`hello world`);
+logger2.log(`hello world`);
+logger2.log(`hello world`);
+logger2.log(`hello world`);
