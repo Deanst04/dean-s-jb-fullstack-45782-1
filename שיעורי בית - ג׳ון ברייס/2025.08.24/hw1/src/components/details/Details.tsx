2cffeeb0d113a9053277a662fcc83b11efc567b7
Dean Stark <deanstark04@gmail.com>
2025-08-26 09:04:33 +0300

    hw 1 - react

---
+++ שיעורי בית - ג׳ון ברייס/2025.08.24/hw1/src/components/details/Details.tsx
@@ -0,0 +1,9 @@
+import './Details.css'
+import img1 from "../assets/images/image1.jpg";
+import img2 from "../assets/images/image2.jpg";
+import img3 from "../assets/images/image3.jpg";
+
+export default function Details() {
+
+    const imagesArr = [img1, img2, img3]
+}
---
+++ שיעורי בית - ג׳ון ברייס/2025.08.24/hw1/src/components/header/Header.tsx
@@ -0,0 +1,9 @@
+import './Header.css'
+
+export default function Header() {
+    return (
+        <header className="Header">
+            <h1>Welcome to my resume website</h1>
+        </header>
+    )
+}
