44542fbb0b9f04bc119279f3547a0d88a418e239
Dean Stark <deanstark04@gmail.com>
2025-09-09 11:57:56 +0300

    not working code

---
+++ קודים ללימודים - ג׳ון ברייס/2025.09.09 - socket/02 - social network auth contd/src/components/common/spinner-button/SpinnerButton.tsx
@@ -0,0 +1,21 @@
+import './SpinnerButton.css'
+import SpinnerSource from '../../../assets/loading1.gif'
+
+interface SpinnerButtonProps {
+    buttonText: string,
+    loadingText: string,
+    isSubmitting: boolean
+    onClick?: React.MouseEventHandler<HTMLButtonElement>
+}
+
+export default function SpinnerButton(props: SpinnerButtonProps) {
+
+    const { buttonText, loadingText, isSubmitting, onClick } = props
+
+    return (
+        <div className='SpinnerButton'>
+            {!isSubmitting && <button onClick={onClick}>{buttonText}</button>}
+            {isSubmitting && <span>{loadingText}... <i><img src={SpinnerSource} /></i></span>}
+        </div>
+    )
+}
