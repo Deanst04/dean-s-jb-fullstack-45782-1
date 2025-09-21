3f90e57a9f21a71f9ba93f48578607bcac1e8b61
Dean Stark <deanstark04@gmail.com>
2025-09-21 13:49:21 +0300

    password generator

---
+++ קודים ללימודים - ג׳ון ברייס/2025.09.21 - node.js/07 - password generator/password-generator.js
@@ -0,0 +1,13 @@
+const generatePassword = length => {
+    const possibleCharacters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789'
+
+    let password = ''
+
+    for(let i = 0; i < length; i++) {
+        password += possibleCharacters[Math.floor(Math.random() * (possibleCharacters.length))]
+    }
+
+    return password
+}
+
+module.exports = generatePassword
