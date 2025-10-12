bfe4cf348cc66af9f60dbc9cf7a532f803540b13
Dean Stark <deanstark04@gmail.com>
2025-10-12 17:41:54 +0300

    new comment

---
+++ קודים ללימודים - ג׳ון ברייס/2025.10.05/09 - weezer server sequelize new comment/src/models/Post.ts
@@ -0,0 +1,36 @@
+import { AllowNull, BelongsTo, Column, DataType, Default, ForeignKey, HasMany, Model, PrimaryKey, Table } from "sequelize-typescript";
+import User from "./User";
+import Comment from "./Comment";
+
+@Table({
+    underscored: true
+})
+export default class Post extends Model {
+    @PrimaryKey
+    @Default(DataType.UUIDV4)
+    @Column(DataType.UUID)
+    id: string
+
+    @ForeignKey(() => User)
+    @AllowNull(false)
+    @Column(DataType.UUID)
+    userId: string // -> user_id
+
+    @AllowNull(false)
+    @Column(DataType.STRING)
+    title: string
+
+    @AllowNull(false)
+    @Column(DataType.TEXT)
+    body: string
+
+    @AllowNull(true)
+    @Column(DataType.TEXT)
+    imageUrl: string
+
+    @BelongsTo(() => User)
+    user: User
+
+    @HasMany(() => Comment)
+    comments: Comment[]
+}
