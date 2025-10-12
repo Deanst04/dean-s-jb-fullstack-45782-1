a46f7dcc235332f15e10dbb26c231407062e86ba
Dean Stark <deanstark04@gmail.com>
2025-10-12 16:56:10 +0300

    get following anf followers

---
+++ קודים ללימודים - ג׳ון ברייס/2025.10.05/08 - weezer server sequelize follows/src/routers/profile.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import { createPost, deletePost, getPost, getProfile, updatePost} from "../controllers/profile/controller";
+import validation from "../middlewares/validation";
+import { newPostValidator, updatePostValidator } from "../controllers/profile/validator";
+
+const router = Router()
+
+router.get('/', getProfile)
+router.get('/:id', getPost)
+router.delete('/:id', deletePost)
+router.post('/', validation(newPostValidator), createPost)
+router.patch('/:id', validation(updatePostValidator),updatePost)
+
+export default router
