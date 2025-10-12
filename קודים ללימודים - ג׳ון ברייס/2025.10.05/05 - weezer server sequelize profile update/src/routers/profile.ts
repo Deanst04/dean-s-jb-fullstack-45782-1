d631312dfeade774f20421dc5843db1eb9ab8c40
Dean Stark <deanstark04@gmail.com>
2025-10-12 13:21:04 +0300

    validation to the nation

---
+++ קודים ללימודים - ג׳ון ברייס/2025.10.05/05 - weezer server sequelize profile update/src/routers/profile.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { createPost, deletePost, getPost, getProfile, updatePost} from "../controllers/profile/controller";
+
+const router = Router()
+
+router.get('/', getProfile)
+router.get('/:id', getPost)
+router.delete('/:id', deletePost)
+router.post('/', createPost)
+router.patch('/:id', updatePost)
+
+export default router
