2d13a79b70bf8db5c7735dfee67a7398ef4b0f8f
Dean Stark <deanstark04@gmail.com>
2025-10-26 13:06:40 +0200

    react infra

--- JohnBryce-Homework/2025.10.21/hw 03/frontend/src/components/layout/header/Header.tsx
+++ JohnBryce-Homework/2025.10.21/hw 03/frontend/src/components/layout/header/Header.tsx
@@ -1,10 +1,12 @@
-import { NavLink } from 'react-router-dom'
 import './Header.css'
 
 export default function Header() {
     return (
         <div className='Header'>
-            
+            <div>am:pm logo</div>
+            <div>
+                AM:PM
+            </div>
         </div>
     )
 }
--- JohnBryce-Homework/2025.10.21/hw 03/frontend/src/components/layout/main/Main.tsx
+++ JohnBryce-Homework/2025.10.21/hw 03/frontend/src/components/layout/main/Main.tsx
@@ -1,11 +1,7 @@
-import { Navigate, Route, Routes } from "react-router-dom";
-
-
 export default function Main() {
     return (
-        <Routes>
-            {/* <Route path="/" element={<Profile />} /> */}
-            
-        </Routes>
+        <div className="Main">
+            main
+        </div>
     )
 }
