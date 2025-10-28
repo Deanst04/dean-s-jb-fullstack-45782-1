9013dd0ebe01a596d7b41c83c635917b23581521
Dean Stark <deanstark04@gmail.com>
2025-10-28 09:10:00 +0200

    some states

--- JohnBryce-Homework/2025.10.21/hw 03/frontend/src/components/layout/main/Main.tsx
+++ JohnBryce-Homework/2025.10.21/hw 03/frontend/src/components/layout/main/Main.tsx
@@ -1,4 +1,12 @@
+import { useState } from "react"
+import type Category from "../../../models/category"
+
 export default function Main() {
+
+    const [categories, setCategories] = useState<Category[]>([])
+    const [selectedCategory, setSelectedCategory] = useState<number | null>(null)
+    
+
     return (
         <div className="Main">
             main
