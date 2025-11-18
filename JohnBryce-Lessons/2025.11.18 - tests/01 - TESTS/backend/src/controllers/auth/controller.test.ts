050dca14a1cdb58b3497aa7853450b50991b082c
Dean Stark <deanstark04@gmail.com>
2025-11-18 11:15:03 +0200

    hashandsalt test

--- JohnBryce-Lessons/2025.11.18 - tests/01 - TESTS/backend/src/controllers/auth/controller.test.ts
+++ JohnBryce-Lessons/2025.11.18 - tests/01 - TESTS/backend/src/controllers/auth/controller.test.ts
@@ -12,5 +12,19 @@ describe('unit testing for all auth controller functions', () => {
             // check that all chars are hexa
             expect(result).toMatch(/^[a-f0-9A-F]+$/)
         })
+        test('same plain text generates same hash', () => {
+            const plainTextPassword = randomUUID()
+            const result1 = hashAndSaltPassword(plainTextPassword)
+            const result2 = hashAndSaltPassword(plainTextPassword)
+            expect(result1).toEqual(result2)
+        })
+        test('different plain text generates different hash', () => {
+            const plainTextPassword1 = randomUUID()
+            const plainTextPassword2 = randomUUID()
+            expect(plainTextPassword1).not.toEqual(plainTextPassword2)
+            const result1 = hashAndSaltPassword(plainTextPassword1)
+            const result2 = hashAndSaltPassword(plainTextPassword2)
+            expect(result1).not.toEqual(result2)
+        })
     })
 })
