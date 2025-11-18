50d193ad4db6b9066bd997b56faabe9788777d66
Dean Stark <deanstark04@gmail.com>
2025-11-18 12:14:11 +0200

    testing happy path

--- JohnBryce-Lessons/2025.11.18 - tests/01 - TESTS/backend/src/middlewares/enforce-auth.test.ts
+++ JohnBryce-Lessons/2025.11.18 - tests/01 - TESTS/backend/src/middlewares/enforce-auth.test.ts
@@ -1,5 +1,8 @@
+import { randomUUID } from "crypto";
 import enforceAuth from "./enforce-auth"
 import { NextFunction, Request, Response } from "express";
+import { sign } from "jsonwebtoken";
+import config from 'config';
 
 describe('enforce auth unit testing', () => {
     test('calls next with status 401 when authorization header missing', () => {
@@ -23,10 +26,7 @@ describe('enforce auth unit testing', () => {
     test('calls next with status 401 when Bearer keyword is misspelled', () => {
         // mocking 
         const request = {
-            headers: {
-                authorization: 'Bearer xmkwmxkwmxmwkmxkwkxmkwmwkmx'
-            },
-            get: (key: string) => 'Bearer xmkwmxkwmxmwkmxkwkxmkwmwkmx'
+            get: (key: string) => 'Baerer xmkwmxkwmxmwkmxkwkxmkwmwkmx'
         } as Request
         const response = {} as Response
         const next = jest.fn(err => {})
@@ -36,6 +36,36 @@ describe('enforce auth unit testing', () => {
             status: 401,
             message: 'missing Bearer keyword'
         })
+
+        // [ [ { status: 401, message: '' } ] ]
+    })
+    test('calls next with status 401 when jwt is missing', () => {
+        // mocking 
+        const request = {
+            get: (key: string) => 'Bearer'
+        } as Request
+        const response = {} as Response
+        const next = jest.fn(err => {})
+        enforceAuth(request, response, next)
+        expect(next.mock.calls.length).toBe(1)
+        expect(next.mock.calls[0][0]).toEqual({
+            status: 401,
+            message: 'missing jwt'
+        })
+
+        // [ [ { status: 401, message: '' } ] ]
+    })
+    test('success when all is valid', () => {
+        // mocking 
+        const jwt = sign(randomUUID(), config.get<string>('app.jwtSecret'))
+        const request = {
+            get: (key: string) => `Bearer ${jwt}`
+        } as Request
+        const response = {} as Response
+        const next = jest.fn(err => {})
+        enforceAuth(request, response, next)
+        expect(next.mock.calls.length).toBe(1)
+        expect(next.mock.calls[0][0]).toBeUndefined()
 
         // [ [ { status: 401, message: '' } ] ]
     })
