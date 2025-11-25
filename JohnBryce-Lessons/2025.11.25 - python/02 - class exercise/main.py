781acefa687c10e461b3c5c5f8d10cea6f702e0f
Dean Stark <deanstark04@gmail.com>
2025-11-25 15:45:48 +0200

    some py updating

--- JohnBryce-Lessons/2025.11.25 - python/02 - class exercise/main.py
+++ JohnBryce-Lessons/2025.11.25 - python/02 - class exercise/main.py
@@ -1,9 +1,18 @@
-import count
+import helpers.count as count
 from icecream import ic
+# from flask import flask
+import json
+
 
 def welcomeMsg():
     print('welcome!')
 
+file = open('requirements.txt')
+
 if __name__ == '__main__':
+    with open("demofile.txt", "a") as f:
+        f.write('now the file as more content')
+    f = open('demofile.txt')
     welcomeMsg()
     ic(count.countString('hello world'))
+    ic(print(file.read()))
