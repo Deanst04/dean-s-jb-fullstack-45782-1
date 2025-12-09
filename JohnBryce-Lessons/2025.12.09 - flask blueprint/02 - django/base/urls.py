b80dda86abdaf2453a1541cd3124872a0bc8cbe6
Dean Stark <deanstark04@gmail.com>
2025-12-09 12:11:03 +0200

    methods

--- JohnBryce-Lessons/2025.12.09 - flask blueprint/02 - django/base/urls.py
+++ JohnBryce-Lessons/2025.12.09 - flask blueprint/02 - django/base/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('hello', views.hello)
+    path('hello', views.hello),
+    path('index', views.index)
 ]
--- JohnBryce-Lessons/2025.12.09 - flask blueprint/02 - django/base/views.py
+++ JohnBryce-Lessons/2025.12.09 - flask blueprint/02 - django/base/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+
+@api_view(['GET'])
 def hello(req):
-  return JsonResponse('hello...', safe=False)
+  return Response('hello')
 
+@api_view(['GET'])
 def index(req):
-  return JsonResponse('index...', safe=False)
+  return Response('index')
