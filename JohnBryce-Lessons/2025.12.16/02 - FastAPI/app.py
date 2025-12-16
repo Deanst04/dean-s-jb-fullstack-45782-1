a45fb33aeda73035a2dd26d583a31c1932de0859
Dean Stark <deanstark04@gmail.com>
2025-12-16 21:50:55 +0200

    Enhance FastAPI user management with update and delete endpoints; update models and requirements

--- JohnBryce-Lessons/2025.12.16/02 - FastAPI/app.py
+++ JohnBryce-Lessons/2025.12.16/02 - FastAPI/app.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from sqlmodel import Session, select
 from database import create_db_and_tables, engine 
-from models import User, UserCreate
+from models import User, UserCreate, UserUpdate
 from contextlib import asynccontextmanager
 
 @asynccontextmanager
@@ -22,9 +22,9 @@ async def root():
 @app.post("/add_user")
 def create_user(user: UserCreate):
     with Session(engine) as session:
-        user = User(
+        user = UserCreate(
             name = user.name,
-            email= user.email
+            last_name = user.last_name
         )
 
         session.add(user)
@@ -52,7 +52,30 @@ def get_user_by_id(id: int):
         return user
 
 
-# @app.patch("/edit_user/{id}")
-# def edit_user_by_id(id: int):
-#     with Session(engine) as session:
-#         user = session.get(User, id)
+@app.patch("/edit_user/{id}")
+def edit_user_by_id(id: int, user: UserUpdate):
+    with Session(engine) as session:
+        db_user = session.get(User, id)
+        if not db_user:
+            return {"error": "User not found"}
+
+        if user.name is not None:
+            db_user.name = user.name
+
+        if user.last_name is not None:
+            db_user.last_name = user.last_name
+
+        session.commit()
+        session.refresh(db_user)
+        return db_user
+
+
+@app.delete("/delete_user/{id}")
+def delete_user_by_id(id: int):
+    with Session(engine) as session:
+        db_user = session.get(User, id)
+        if not db_user:
+            return {"error": "User not found"}
+        session.delete(db_user)
+        session.commit()
+        return {"success": "user deleted successfully"}
--- JohnBryce-Lessons/2025.12.16/02 - FastAPI/models.py
+++ JohnBryce-Lessons/2025.12.16/02 - FastAPI/models.py
@@ -1,11 +1,14 @@
 from sqlmodel import Field, SQLModel
-from typing import Optional
 
 class User(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
+    id: int | None = Field(default=None, primary_key=True)
     name: str
-    email: str
+    last_name: str
 
 class UserCreate(SQLModel):
     name: str
-    email: str
+    last_name: str
+
+class UserUpdate(SQLModel):
+    name: str | None = None
+    last_name: str | None = None
