f257d1ff284e16e6645352cb8603f4aa2585d3fa
Dean Stark <deanstark04@gmail.com>
2026-01-04 12:23:56 +0200

    first angular proj

---
+++ JohnBryce-Lessons/2026.01.04/01 - ANGULAR/angular-first-app/src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { About } from './about/about';
+import { Home } from './home/home';
+import { Contact } from './contact/contact';
+
+export const routes: Routes = [
+    { path: '', component: Home },
+    { path: 'about', component: About },
+    { path: 'contact', component: Contact }
+];
